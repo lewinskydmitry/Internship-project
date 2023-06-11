374eefa62fa4f018e191e62891be7376985349e2
lewinskydmitry <61576391+lewinskydmitry@users.noreply.github.com>
2023-06-11 13:10:35 +0200

    update joined model

--- src/Models/Classifiers.py
+++ src/Models/Classifiers.py
@@ -51,10 +51,17 @@ class Simple_classifier(nn.Module):
     def __init__(self, num_features):
         super(Simple_classifier, self).__init__()
 
-        self.layer1 = nn.Linear(num_features, 2)
+        self.layer1 = nn.Sequential(
+            nn.Linear(num_features),
+            nn.BatchNorm1d(num_features),
+            nn.ReLU()
+        )
+
+        self.layer2 = nn.Linear(num_features, 2)
 
     def forward(self, x):
         x = self.layer1(x)
+        x = self.layer2(x)
         return x
 
 ### LOSS FUNCTION ###
