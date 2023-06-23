911ad7e38f533d6c24a5993f0f5851f5af72038c
Dmitrii <dima.fomin.2013@mail.ru>
2023-06-23 19:06:22 +0200

    finish data sampler

--- src/imbalanced_learing/data_level.py
+++ src/imbalanced_learing/data_level.py
@@ -3,22 +3,19 @@ import numpy as np
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import OneSidedSelection
 
-class data_level():
-    def __init__(self, X, y, p, random_seed) -> None:
+class data_sampler():
+    def __init__(self, X, y) -> None:
         self.X = X
         self.y = y
         self.data = {}
-        self.p = p
-        self.randoom_seed = random_seed
-        np.random.seed(random_seed)
 
-
-    def prepare_data(self, X, y):
+    
+    def copy_data(self, X, y):
         return np.array(X).copy(), np.array(y).copy()
-    
-    
+
+
     def ROS(self, p=1.):
-        X, y = self.prepare_data(self.X, self.y)
+        X, y = self.copy_data(self.X, self.y)
 
         class_counts = np.bincount(y)
         minority_class = np.argmin(class_counts)
@@ -26,11 +23,12 @@ class data_level():
         minority_indices = np.where(y == minority_class)[0]
 
         sampling_amount =  int(class_counts[majority_class] * p) - class_counts[minority_class]
-        minority_indices = np.where(y == minority_class)[0]
         oversampled_indices = np.random.choice(minority_indices, size=sampling_amount, replace=True)
 
         X_oversampled = np.concatenate((X, X[oversampled_indices]), axis=0)
         y_oversampled = np.concatenate((y, y[oversampled_indices]), axis=0)
+        y_oversampled = y_oversampled.reshape(-1,1)
+
         ROS_data = np.concatenate((X_oversampled, y_oversampled), axis = 1)
         np.random.shuffle(ROS_data)
         self.data['ROS_data'] = ROS_data
@@ -38,8 +36,8 @@ class data_level():
         return ROS_data
     
 
-    def RUS(self, p=0.5):
-        X, y = self.prepare_data(self.X, self.y)
+    def RUS(self, p=1.):
+        X, y = self.copy_data(self.X, self.y)
 
         class_counts = np.bincount(y)
         minority_class = np.argmin(class_counts)
@@ -47,11 +45,11 @@ class data_level():
         majority_indices = np.where(y != minority_class)[0]
 
         sampling_amount =  class_counts[majority_class] - int(class_counts[minority_class] / p)
-
         undersampled_indices = np.random.choice(majority_indices, size=sampling_amount, replace=False)
 
         X_undersampled = np.concatenate((X[~undersampled_indices], X[y == minority_class]), axis=0)
         y_undersampled = np.concatenate((y[~undersampled_indices], y[y == minority_class]), axis=0)
+        y_undersampled = y_undersampled.reshape(-1,1)
 
         RUS_data = np.concatenate((X_undersampled, y_undersampled), axis = 1)
         np.random.shuffle(RUS_data)
@@ -61,7 +59,7 @@ class data_level():
 
 
     def SMOTE(self, p = 1.):
-        X, y = self.prepare_data(self.X, self.y)
+        X, y = self.copy_data(self.X, self.y)
         sm = SMOTE(random_state=42, sampling_strategy = p) # type: ignore
         X_res, y_res = sm.fit_resample(X, y) # type: ignore
         y_res = np.array(y_res).reshape(-1,1)
@@ -71,8 +69,9 @@ class data_level():
 
         return SMOTE_data
 
+
     def OSS(self):
-        X, y = self.prepare_data(self.X, self.y)
+        X, y = self.copy_data(self.X, self.y)
         oss = OneSidedSelection(random_state=42)
         X_res, y_res = oss.fit_resample(X, y) # type: ignore
         y_res = np.array(y_res).reshape(-1,1)
@@ -81,3 +80,9 @@ class data_level():
         self.data['OSS_data'] = OSS_data
 
         return OSS_data
+    
+    def make_data(self,p = 1.):
+        self.ROS(p)
+        self.RUS(p)
+        self.SMOTE(p)
+        self.OSS()
