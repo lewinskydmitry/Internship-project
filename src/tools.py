8cf89fe518d676a4a465b60dcb0f4996ad758144
Dmitrii <dima.fomin.2013@mail.ru>
2023-06-29 09:09:44 +0200

    update sampler

--- src/tools.py
+++ src/tools.py
@@ -6,46 +6,7 @@ from catboost import CatBoostClassifier
 from tqdm import tqdm
 from catboost import Pool
 from sklearn.model_selection import train_test_split
-from torch.utils.data import Subset
-
-
-def upsampling(p=1., *data):
-  if len(data) == 1:
-    # Count the number of samples in each class
-    df = data[0]
-    class_counts = df['Machine failure'].value_counts()
-
-    # Determine the minority and majority class
-    minority_class = class_counts.idxmin()
-    majority_class = class_counts.idxmax()
-
-    # Upsample the minority class with replacement
-    minority_df = df[df['Machine failure'] == minority_class]
-    upsampled_df = pd.concat(
-        [df] + [minority_df.sample(n=int((class_counts[majority_class] - class_counts[minority_class])*p),
-                                  replace=True, random_state = 0)], axis=0)
-
-    # Shuffle the upsampled data
-    upsampled_df = upsampled_df.sample(frac=1, random_state = 0).reset_index(drop = True)
-    return upsampled_df
-  else:
-    df = pd.concat([data[0], data[1]], axis=1)
-    # Count the number of samples in each class
-    class_counts = df['Machine failure'].value_counts()
-
-    # Determine the minority and majority class
-    minority_class = class_counts.idxmin()
-    majority_class = class_counts.idxmax()
-
-    # Upsample the minority class with replacement
-    minority_df = df[df['Machine failure'] == minority_class]
-    upsampled_df = pd.concat(
-        [df] + [minority_df.sample(n=int((class_counts[majority_class] - class_counts[minority_class])*p),
-                                  replace=True, random_state = 0)], axis=0)
-
-    # Shuffle the upsampled data
-    upsampled_df = upsampled_df.sample(frac=1, random_state = 0).reset_index(drop = True)
-    return upsampled_df.drop(columns = ['Machine failure']), upsampled_df['Machine failure']  
+from src.sampling_methods.sampler import DataSampler
 
 
 def check_result(model,X_test,y_test):
@@ -58,9 +19,10 @@ def check_result(model,X_test,y_test):
   return metrics_dict
 
 
-def search_num_features(df, feature_importance, upsamp_func = False, step = 5):
+def search_num_features(df, feature_importance, upsamp_func = 'ROS', p = 0.3, step = 5):
   best_score = 0
   best_num_features = 0
+
   for num_col in tqdm(range(1, len(feature_importance), step)):
     features = list(feature_importance.iloc[:num_col,:]['feature_names'])
     data = df[features + ['Machine failure']]
@@ -70,7 +32,7 @@ def search_num_features(df, feature_importance, upsamp_func = False, step = 5):
                                                         random_state=42,
                                                         stratify = df['Machine failure'])
     if upsamp_func == True:
-      X_train, y_train = upsampling(1, X_train, y_train)
+      X_train, y_train = DataSampler.ROS(X_train, y_train,)
 
     train_pool = Pool(data=X_train, label=y_train)
     CatBoost = CatBoostClassifier(verbose=False,random_seed=42)
@@ -82,16 +44,3 @@ def search_num_features(df, feature_importance, upsamp_func = False, step = 5):
       best_num_features = num_col
   print(f'Best F1_score - {best_score}, num_features - {best_num_features}')
 
-
-def balance_val_split(dataset, train_size=0.7):
-    targets = np.array(dataset.targets)
-    train_indices, val_indices = train_test_split(
-        np.arange(targets.shape[0]),
-        shuffle=True,
-        train_size=train_size,
-        stratify=targets
-    )
-
-    train_dataset = Subset(dataset, indices=train_indices)
-    val_dataset = Subset(dataset, indices=val_indices)
-    return train_dataset, val_dataset
