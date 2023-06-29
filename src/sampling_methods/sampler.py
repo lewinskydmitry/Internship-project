eeb82731a670c61f8dd91b1965ff30bd36c0f1ff
Dmitrii <dima.fomin.2013@mail.ru>
2023-06-29 09:10:08 +0200

    update sampler

--- src/sampling_methods/sampler.py
+++ src/sampling_methods/sampler.py
@@ -3,17 +3,14 @@ import numpy as np
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import OneSidedSelection
 
-class DataSampler():
-    def __init__(self, X, y) -> None:
-        pass
-
-
-    def copy_data(self, X, y):
+def copy_data(X, y):
         return np.array(X).copy(), np.array(y).copy()
 
+class DataSampler():
 
-    def ROS(self, X, y, p=1.):
-        X, y = self.copy_data(X, y)
+    @staticmethod
+    def ROS(X, y, p=1.):
+        X, y = copy_data(X, y)
 
         class_counts = np.bincount(y)
         minority_class = np.argmin(class_counts)
@@ -32,9 +29,9 @@ class DataSampler():
 
         return ROS_data
     
-
-    def RUS(self, X, y, p=1.):
-        X, y = self.copy_data(X, y)
+    @staticmethod
+    def RUS(X, y, p=1.):
+        X, y = copy_data(X, y)
 
         class_counts = np.bincount(y)
         minority_class = np.argmin(class_counts)
@@ -53,9 +50,9 @@ class DataSampler():
 
         return RUS_data
 
-
-    def SMOTE(self, X, y, p = 1.):
-        X, y = self.copy_data(self.X, self.y)
+    @staticmethod
+    def SMOTE(X, y, p = 1.):
+        X, y = copy_data(X, y)
         sm = SMOTE(random_state=42, sampling_strategy = p) # type: ignore
         X_res, y_res = sm.fit_resample(X, y) # type: ignore
         y_res = np.array(y_res).reshape(-1,1)
@@ -64,9 +61,9 @@ class DataSampler():
 
         return SMOTE_data
 
-
-    def OSS(self, X, y):
-        X, y = self.copy_data(X, y)
+    @staticmethod
+    def OSS(X, y):
+        X, y = copy_data(X, y)
         oss = OneSidedSelection(random_state=42)
         X_res, y_res = oss.fit_resample(X, y) # type: ignore
         y_res = np.array(y_res).reshape(-1,1)
